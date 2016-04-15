2958e540a4ebe883d2b2a0f5ac21fd06f136b1c1
pritansh <pritansh.chaudhary@gmail.com>
2016-04-16 01:58:19 +0530

    Created shell script

---
+++ CompilerInJava.sh
@@ -0,0 +1,20 @@
+set+v
+if [ ! -e code.pc ];then
+cp Compiler/code.pc .
+fi
+if [ -e classes ]; then
+rmdir classes -p
+fi
+mkdir classes
+mkdir classes/src
+mkdir classes/lib
+javac -cp Parser/lib/antlr.jar -d classes/src/ Parser/src/pc/parser/*.java
+javac -cp Parser/lib/antlr.jar;Parser/src/;Compiler/src/ -d classes/src/ Compiler/src/pc/compiler/*.java
+cp code.pc classes/src/
+cp Compiler/lib/jasmin.jar classes/lib/
+cp Parser/lib/antlr.jar classes/lib/
+cd classes/src/
+java -cp .;../lib/antlr.jar pc.compiler.Main
+java -jar ../lib/jasmin.jar Demo.j
+java Demo
+sleep
